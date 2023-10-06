44fa29f56c3a5c35a1333a269db8366d0f6c9c8b
cyh272036 <86253660+cyh272036@users.noreply.github.com>
2023-10-06 17:14:30 +0900

    2023/10/6

---
+++ data2.js
@@ -0,0 +1,35 @@
+let data2 = [
+
+    {
+
+        id: 1,
+        title: "2023년 수원FC AWAY 레플리카 유니폼",
+        imgUrl: "/img/goods_10.png",
+        content: "a",
+        price: 80000
+
+    },
+
+    {
+
+        id: 2,
+        title: "수원FC 23 홈 머플러",
+        imgUrl: "/img/goods_11.png",
+        content: "c",
+        price: 25000
+
+    },
+
+    {
+
+        id: 3,
+        title: "수원FC 23 사인볼",
+        imgUrl: "/img/goods_12.png",
+        content: "b",
+        price: 20000
+
+    },
+]
+
+
+export default data2; 
---
+++ data3.js
@@ -0,0 +1,35 @@
+let data3 = [
+
+    {
+
+        id: 1,
+        title: "2023년 수원FC GK AWAY 레플리카 유니폼",
+        imgUrl: "/img/goods_13.png",
+        content: "a",
+        price: 80000
+
+    },
+
+    {
+
+        id: 2,
+        title: "수원FC 20주년 아크릴 키링",
+        imgUrl: "/img/goods_14.png",
+        content: "b",
+        price: 8000
+
+    },
+
+    {
+
+        id: 3,
+        title: "수원FC 무릎담요",
+        imgUrl: "/img/goods_15.png",
+        content: "b",
+        price: 22000
+
+    },
+]
+
+
+export default data3; 
